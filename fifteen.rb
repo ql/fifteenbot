7a524e7bb0ba3c435f1bc852c61908bf1a946600
Alexander Sviridov <alexander.sviridov@gmail.com>
2016-12-02 22:36:26 +0300

    slide

--- fifteen.rb
+++ fifteen.rb
@@ -1,4 +1,52 @@
 class Fifteen
-  def initialize
+  def initialize(numbers = nil)
+    numbers ||= ((1..15).to_a + ["_"]).sort_by { rand }
+    @field = []
+    while numbers.any?
+      @field << numbers.slice!(0, 4)
+    end
+  end
+
+  def [](row)
+    @field[row]
+  end
+
+  def inspect
+    @field.map { |row| row.map { |val| val.to_s.rjust(2, " ") }.join(" ") }.join("\n")
+  end
+
+  def slide!(row, col)
+    e_row, e_col = empty_pos
+    if row == e_row
+      new_fif = Fifteen.new(@field.flatten)
+      if e_col < col
+        (e_col...col).each { |i| new_fif[row][i] = new_fif[row][i+1] }
+        new_fif[row][col] = '_'
+      elsif e_col > col
+        (e_col.downto(col)).each { |i| new_fif[row][i] = new_fif[row][i-1] }
+        new_fif[row][col] = '_'
+      end
+      new_fif
+    elsif e_col == col
+      new_fif = Fifteen.new(@field.flatten)
+      if e_row < row
+        (e_row...row).each { |i| new_fif[i][col] = new_fif[i+1][col] }
+        new_fif[row][col] = '_'
+      elsif e_row > row
+        (e_row.downto(row)).each { |i| new_fif[i][col] = new_fif[i-1][col] }
+        new_fif[row][col] = '_'
+      end
+      new_fif
+    else
+      self
+    end
+  end
+
+  def empty_pos
+    [0, 1, 2, 3].permutation(2).detect { |row, col| self[row][col] == '_' }
+  end
+
+  def set_empty(row, col)
+    self[row][col] = '_'
   end
 end
--- fifteen_test.rb
+++ fifteen_test.rb
@@ -1,9 +1,72 @@
 require './spec_helper.rb'
 
 RSpec.describe Fifteen do
-  describe "#new" do
+  before(:each) do
+    @fifteen = Fifteen.new([5, 3, 11, 8, 15, 10, 2, 6, "_", 14, 9, 7, 4, 13, 1, 12])
+  end
+
+  describe "#inspect" do
     it "should be" do
-      puts Fifteen.new
+      expect(@fifteen.inspect).to eq %Q( 5  3 11  8
+15 10  2  6
+ _ 14  9  7
+ 4 13  1 12)
+    end
+  end
+
+  describe "#empty_pos" do
+    it "should work" do
+      expect(@fifteen.empty_pos).to eq([2, 0])
+    end
+  end
+
+
+  describe "#slide" do
+    it "should work" do
+      expect(@fifteen.slide!(0,0)).not_to eq(@fifteen)
+      expect(@fifteen.slide!(1,0)).not_to eq(@fifteen)
+      expect(@fifteen.slide!(2,0)).not_to eq(@fifteen)
+      expect(@fifteen.slide!(3,0)).not_to eq(@fifteen)
+
+      expect(@fifteen.slide!(0,1)).to eq(@fifteen)
+      expect(@fifteen.slide!(1,1)).to eq(@fifteen)
+      expect(@fifteen.slide!(2,1)).not_to eq(@fifteen)
+      expect(@fifteen.slide!(3,1)).to eq(@fifteen)
+
+      expect(@fifteen.slide!(0,2)).to eq(@fifteen)
+      expect(@fifteen.slide!(1,2)).to eq(@fifteen)
+      expect(@fifteen.slide!(2,2)).not_to eq(@fifteen)
+      expect(@fifteen.slide!(3,2)).to eq(@fifteen)
+
+      expect(@fifteen.slide!(0,3)).to eq(@fifteen)
+      expect(@fifteen.slide!(1,3)).to eq(@fifteen)
+      expect(@fifteen.slide!(2,3)).not_to eq(@fifteen)
+      expect(@fifteen.slide!(3,3)).to eq(@fifteen)
+    end
+
+    it "should alter state col" do
+      puts @fifteen.inspect
+      puts
+      puts @fifteen.slide!(2,1).inspect
+      puts
+      puts @fifteen.slide!(2,2).inspect
+      puts
+      puts @fifteen.slide!(2,3).slide!(2,2).inspect
+      puts
+    end
+
+    it "should alter state row" do
+      puts '===='
+      puts @fifteen.inspect
+      puts
+      puts @fifteen.slide!(0,0).inspect
+      puts
+      puts @fifteen.slide!(1,0).inspect
+      puts
+      puts @fifteen.slide!(2,0).inspect
+      puts
+      puts @fifteen.slide!(3,0).inspect
+      puts
     end
   end
 end
